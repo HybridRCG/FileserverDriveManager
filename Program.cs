007b8982e0e519a02b353f890b9f9552f5dd144b
HybridRCG <HybridRCG@users.noreply.github.com>
2026-05-03 10:56:54 +0200

    Remove redundant dotnet build step that was producing framework-dependent artifacts that publish was reusing - bump v4.5

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace FileserverDriveManager
 
     public partial class MainForm : Form
     {
-        private const string APP_VERSION = "v4.4";        
+        private const string APP_VERSION = "v4.5";        
         private List<DriveMapping> drives = new List<DriveMapping>();
         private TextBox usernameBox;
         private TextBox passwordBox;
